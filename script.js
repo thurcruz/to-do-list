162a03bb50f49a700b2139be16e0234653916b97
thurcruz <arthurindcruz@gmail.com>
2024-11-05 23:11:14 -0300

    add edit-task fuction

--- script.js
+++ script.js
@@ -37,16 +37,16 @@ $(document).ready(function() {
     function addTaskToDOM(taskText, completed = false) {
         const taskItem = $("<li></li>").addClass("todo-item");
         const taskSpan = $("<span></span>").text(taskText);
-        const deleteBtn = $("<button></button>")
-            .addClass("delete-task")
-            .html("<ion-icon name='trash-outline'></ion-icon>"); // Ícone da lixeira
-
+        const deleteBtn = $("<button></button>").addClass("delete-task").html("<ion-icon name='trash-outline'></ion-icon>");
+        const editBtn = $("<button></button>").addClass("edit-task").html("<ion-icon name='pencil-outline'></ion-icon>");
+        
         if (completed) {
             taskItem.addClass("completed");
         }
 
         taskItem.append($("<input type='checkbox'>").prop("checked", completed))
                 .append(taskSpan)
+                .append(editBtn)
                 .append(deleteBtn);
         $("#task-list").append(taskItem);
     }
@@ -63,27 +63,52 @@ $(document).ready(function() {
 
     // Adicionar tarefa pressionando Enter
     $("#task-input").keypress(function(event) {
-        if (event.which === 13) { // Código da tecla Enter
+        if (event.which === 13) {
             $("#add-task").click(); // Simula o clique no botão
         }
     });
 
-    // Marcar como concluída ou remover
-    $("#task-list").on("change", "input[type='checkbox']", function() {
-        $(this).closest(".todo-item").toggleClass("completed");
+    // Editar tarefa
+    $("#task-list").on("click", ".edit-task", function() {
+        const taskItem = $(this).closest(".todo-item");
+        const taskText = taskItem.find("span").text();
+
+        // Substitui o texto da tarefa por um input para edição
+        const inputEdit = $("<input>").addClass("task-input-edit").val(taskText);
+
+        // Substitui o span pela input
+        taskItem.find("span").replaceWith(inputEdit);
+
+        // Troca o botão de editar para salvar
+        $(this).html("<ion-icon name='checkmark-outline'></ion-icon>").removeClass("edit-task").addClass("save-task");
+    });
+
+    // Salvar edição de tarefa
+    $("#task-list").on("click", ".save-task", function() {
+        const taskItem = $(this).closest(".todo-item");
+        const editedText = taskItem.find(".task-input-edit").val();
+
+        // Substitui o input pelo novo texto
+        const taskSpan = $("<span></span>").text(editedText);
+        taskItem.find(".task-input-edit").replaceWith(taskSpan);
+
+        // Troca o botão de salvar para editar novamente
+        $(this).html("<ion-icon name='pencil-outline'></ion-icon>").removeClass("save-task").addClass("edit-task");
+
         saveTasks();
     });
 
+    // Excluir tarefa
     $("#task-list").on("click", ".delete-task", function() {
         $(this).closest(".todo-item").remove();
         saveTasks();
     });
 
-    // Mostrar mensagem após 5 segundos
-    setTimeout(function() {
-        $("#message").fadeIn();
-    }, 5000);
+   // Mostrar mensagem após 5 segundos
+   setTimeout(function() {
+    $("#message").fadeIn();
+}, 5000);
 
-    // Carregar tarefas ao carregar a página
+    // Carregar as tarefas ao iniciar
     loadTasks();
 });
